263bbf94300b5f368656ee42a8f8175840e63502
Jeadrik <JeffJ1@wloo.org>
2019-10-30 11:06:45 -0500

    end of class 10/30/19

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ funtion setup() {
     TextSize(18)
 }
 
+function draw() {
+    background(255)
+    displayScore();
+    if(gamePlaying) {
+        addApple();
+        drawSnake();
+     } else {
+        textAlign(CENTER);
+        text('press UP, RIGHT, LEFT, or DOWN arrows to start', width / 2, height / 2);
+        fill(0,255,0);
+        for(var i = 0; i < snakeArray.length; i++) {
+            rect(snakeArray [i].xPos, snakeArray[i].yPos, 19, 19);
+        }
+        addApple();
+    }
+}
+
+function loadBestScore() {
+    var bestScore = getItem('bestScore');
+    if(bestScore) {
+        return bestScore;
+    }
+    return 0;
+}
+
+funtion displayScore() {
+    line(0, 40, width, 40);
+    line(0, 0, 0, 480);
+    line(0, 480, 400, 480);
+    line(400, 480, 400, 0);
+    line(0, 0, 400, 0);
+    fill(0, 102, 153);
+    textAlign(LEFT);
+    text('Score', 30, 27);
+    text(score, 100, 27);
+
+    text('Best Score', 230, 27);
+    text(bestScore, 340, 27);
+}
+
+function makeSnakePiece(x, y) {
+    snakePiece = {
+        xPos: x,
+        uPos: y
+    };
+    append(snakeArray, snakePiece);
+    
+}
